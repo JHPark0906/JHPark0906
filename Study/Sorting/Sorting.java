55ffde32397aad02fd38a9ef1d2b0e935be31f45
JHPark0906 <ejje0906@gmail.com>
2022-12-13 17:25:14 +0900

    sorting

--- Study/Sorting/Sorting.java
+++ Study/Sorting/Sorting.java
@@ -1,23 +1,20 @@
 import java.util.ArrayList;
-import java.util.Collection;
 import java.util.Collections;
-import java.util.stream.Collectors;
-import java.util.stream.Stream;
+import java.util.Comparator;
 
 public class Sorting {
 
-    static final int REPETITION = 100;
+    static final int REPETITION = 1000;
 
     static int count = 0;
 
     static long time = 0;
 
-    static int[] result = new int[32];
-
     static int size;
 
     public static void main(String[] args) {
         int[] inputSize = {100, 200, 500, 1000, 2000, 3000, 4000, 5000};
+        int[] result = new int[40];
         boolean check = true;
 
         for (int i = 0; i < 8; i++) {
@@ -25,9 +22,9 @@ public class Sorting {
             for (int j = 0; j < REPETITION; j++) {
                 ArrayList arr = randomArray(inputSize[i]);
                 long start = System.nanoTime();
-                ArrayList<Integer> result = quicksort(arr);
+                ArrayList<Integer> sorted = quicksort(arr);
                 time += (System.nanoTime() - start);
-                check &= checker(result);
+                check &= checker(sorted);
             }
             result[2 * i] = count / REPETITION;
             result[2 * i + 1] = (int)(time / REPETITION);
@@ -38,19 +35,34 @@ public class Sorting {
             for (int j = 0; j < REPETITION; j++) {
                 ArrayList arr = randomArray(inputSize[i]);
                 long start = System.nanoTime();
-                ArrayList<Integer> result = heapsort(arr);
+                ArrayList<Integer> sorted = heapsort(arr);
                 time += (System.nanoTime() - start);
-                check &= checker(result);
+                check &= checker(sorted);
             }
             result[2 * i + 16] = count / REPETITION;
             result[2 * i + 17] = (int)(time / REPETITION);
         }
 
+        for (int i = 0; i < 8; i++) {
+            init();
+            for (int j = 0; j < REPETITION; j++) {
+                long start = System.nanoTime();
+                randomArray(inputSize[i]).sort(new Comparator<Integer>() {
+                    @Override
+                    public int compare(Integer o1, Integer o2) {
+                        return o2 - o1;
+                    }
+                });
+                time += (System.nanoTime() - start);
+            }
+            result[i + 32] = (int)(time / REPETITION);
+        }
+
         System.out.println(check);
-        System.out.println("Quicksort\t\tHeapsort");
-        System.out.println("Count\tTime\tCount\tTime");
-        for(int i = 0; i < 8; i++) {
-            System.out.println(result[2 * i] + "\t" + result[2 * i + 1] + "\t" + result[2 * i + 16] + "\t" + result[2 * i + 17]);
+        System.out.println("Quicksort\t\tHeapsort\t\t.sort");
+        System.out.println("Count\tTime\tCount\tTime\tTime");
+        for (int i = 0; i < 8; i++) {
+            System.out.println(result[2 * i] + "\t" + result[2 * i + 1] + "\t" + result[2 * i + 16] + "\t" + result[2 * i + 17] + "\t" + result[i + 32]);
         }
     }
 
@@ -81,10 +93,12 @@ public class Sorting {
 
     public static ArrayList<Integer> quicksort(ArrayList<Integer> arr){
         if (arr.size() <= 1) return arr;
+
         int pivot = arr.get(arr.size() / 2);
         ArrayList<Integer> left = new ArrayList<>();
         ArrayList<Integer> equal = new ArrayList<>();
         ArrayList<Integer> right = new ArrayList<>();
+
         for (int n : arr) {
             count += 2;
             if (n < pivot) {
@@ -97,11 +111,18 @@ public class Sorting {
                 equal.add(n);
             }
         }
-        return (ArrayList<Integer>) Stream.of(quicksort(left), equal, quicksort(right)).flatMap(Collection::stream).collect(Collectors.toList());
+
+        ArrayList<Integer> result = new ArrayList<>();
+        result.addAll(quicksort(left));
+        result.addAll(equal);
+        result.addAll(quicksort(right));
+
+        return result;
     }
 
     public static ArrayList<Integer> heapsort(ArrayList<Integer> arr) {
         if (arr.size() <= 1) return arr;
+
         size = arr.size();
         for (int i = size - 1; i >= 0; i--) {
             heapify(arr, i);
@@ -112,6 +133,7 @@ public class Sorting {
             size--;
             heapify(arr, 0);
         }
+
         return arr;
     }
 
