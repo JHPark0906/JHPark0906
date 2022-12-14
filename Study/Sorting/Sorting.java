687b08383a2c3eddce1acafd145c45d2ba1581a4
JHPark0906 <ejje0906@gmail.com>
2022-12-14 21:51:51 +0900

    Add pivot selection.

--- Study/Sorting/Sorting.java
+++ Study/Sorting/Sorting.java
@@ -14,21 +14,54 @@ public class Sorting {
 
     public static void main(String[] args) {
         int[] inputSize = {100, 200, 500, 1000, 2000, 3000, 4000, 5000};
-        int[] result = new int[40];
+        int[] result = new int[72];
         boolean check = true;
+        String pivotChoice;
 
+        pivotChoice = "First";
         for (int i = 0; i < 8; i++) {
             init();
             for (int j = 0; j < REPETITION; j++) {
                 ArrayList arr = randomArray(inputSize[i]);
                 long start = System.nanoTime();
-                ArrayList<Integer> sorted = quicksort(arr);
+                ArrayList<Integer> sorted = quicksort(arr, pivotChoice);
                 time += (System.nanoTime() - start);
                 check &= checker(sorted);
             }
             result[2 * i] = count / REPETITION;
             result[2 * i + 1] = (int)(time / REPETITION);
         }
+        System.out.println("Quicksort that choice first element as pivot is done.");
+
+        pivotChoice = "Random";
+        for (int i = 0; i < 8; i++) {
+            init();
+            for (int j = 0; j < REPETITION; j++) {
+                ArrayList arr = randomArray(inputSize[i]);
+                long start = System.nanoTime();
+                ArrayList<Integer> sorted = quicksort(arr, pivotChoice);
+                time += (System.nanoTime() - start);
+                check &= checker(sorted);
+            }
+            result[2 * i + 16] = count / REPETITION;
+            result[2 * i + 17] = (int)(time / REPETITION);
+        }
+        System.out.println("Quicksort that choice random element as pivot is done.");
+
+        pivotChoice = "Median";
+        for (int i = 0; i < 8; i++) {
+            init();
+            for (int j = 0; j < REPETITION; j++) {
+                ArrayList arr = randomArray(inputSize[i]);
+                long start = System.nanoTime();
+                ArrayList<Integer> sorted = quicksort(arr, pivotChoice);
+                time += (System.nanoTime() - start);
+                check &= checker(sorted);
+            }
+            result[2 * i + 32] = count / REPETITION;
+            result[2 * i + 33] = (int)(time / REPETITION);
+        }
+        System.out.println("Quicksort that choice median element as pivot is done.");
 
         for (int i = 0; i < 8; i++) {
             init();
@@ -39,9 +72,10 @@ public class Sorting {
                 time += (System.nanoTime() - start);
                 check &= checker(sorted);
             }
-            result[2 * i + 16] = count / REPETITION;
-            result[2 * i + 17] = (int)(time / REPETITION);
+            result[2 * i + 48] = count / REPETITION;
+            result[2 * i + 49] = (int)(time / REPETITION);
         }
+        System.out.println("Heapsort is done.");
 
         for (int i = 0; i < 8; i++) {
             init();
@@ -55,14 +89,17 @@ public class Sorting {
                 });
                 time += (System.nanoTime() - start);
             }
-            result[i + 32] = (int)(time / REPETITION);
+            result[i + 64] = (int)(time / REPETITION);
         }
+        System.out.println("Using sort method is done.");
 
         System.out.println(check);
-        System.out.println("Quicksort\t\tHeapsort\t\t.sort");
-        System.out.println("Count\tTime\tCount\tTime\tTime");
+        System.out.println("QS(First)\t\tQS(Random)\t\tQS(Median)\t\tHeapsort\t\t.sort");
+        System.out.println("Count\tTime\tCount\tTime\tCount\tTime\tCount\tTime\tTime");
         for (int i = 0; i < 8; i++) {
-            System.out.println(result[2 * i] + "\t" + result[2 * i + 1] + "\t" + result[2 * i + 16] + "\t" + result[2 * i + 17] + "\t" + result[i + 32]);
+            System.out.println(result[2 * i] + "\t" + result[2 * i + 1] + "\t" + result[2 * i + 16] + "\t"
+                    + result[2 * i + 17] + "\t" + result[2 * i + 32] + "\t" + result[2 * i + 33] + "\t"
+                    + result[2 * i + 48] + "\t" + result[2 * i + 49] + "\t" + result[i + 64]);
         }
     }
 
@@ -91,10 +128,46 @@ public class Sorting {
         return result;
     }
 
-    public static ArrayList<Integer> quicksort(ArrayList<Integer> arr){
+    public static ArrayList<Integer> quicksort(ArrayList<Integer> arr, String pivotChoice){
         if (arr.size() <= 1) return arr;
+        int index = 0;
+        int pivot;
 
-        int pivot = arr.get(arr.size() / 2);
+        switch (pivotChoice) {
+            case "First" -> index = 0;
+            case "Random" -> index = (int) (Math.random() * arr.size());
+            case "Median" -> {
+                count++;
+                if (arr.get(0) <= arr.get(arr.size() / 2)) {
+                    count++;
+                    if (arr.get(arr.size() / 2) <= arr.get(arr.size() - 1)) {
+                        index = arr.size() / 2;
+                    } else {
+                        count++;
+                        if (arr.get(0) <= arr.get(arr.size() - 1)) {
+                            index = arr.size() - 1;
+                        } else {
+                            index = 0;
+                        }
+                    }
+                } else {
+                    count++;
+                    if (arr.get(arr.size() / 2) <= arr.get(arr.size() - 1)) {
+                        count++;
+                        if (arr.get(0) <= arr.get(arr.size() - 1)) {
+                            index = 0;
+                        } else {
+                            index = arr.size() - 1;
+                        }
+                    } else {
+                        index = arr.size() / 2;
+                    }
+                }
+            }
+        }
+
+        pivot = arr.get(index);
+
         ArrayList<Integer> left = new ArrayList<>();
         ArrayList<Integer> equal = new ArrayList<>();
         ArrayList<Integer> right = new ArrayList<>();
@@ -113,9 +186,9 @@ public class Sorting {
         }
 
         ArrayList<Integer> result = new ArrayList<>();
-        result.addAll(quicksort(left));
+        result.addAll(quicksort(left, pivotChoice));
         result.addAll(equal);
-        result.addAll(quicksort(right));
+        result.addAll(quicksort(right, pivotChoice));
 
         return result;
     }
