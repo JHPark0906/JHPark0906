fc02acfea2a3a8a962bca559ce93a92f9bdd1968
JHPark0906 <91676766+JHPark0906@users.noreply.github.com>
2023-03-31 11:11:50 +0900

    [Bronze IV] Title: I Speak TXTMSG, Time: 40 ms, Memory: 31256 KB -BaekjoonHub

---
+++ 백준/Bronze/6841. I Speak TXTMSG/I Speak TXTMSG.py
@@ -0,0 +1,29 @@
+while True:
+    str = input()
+    if str == "CU":
+        print("see you")
+    elif str == ":-)":
+        print("I’m happy")
+    elif str == ":-(":
+        print("I’m unhappy")
+    elif str == ";-)":
+        print("wink")
+    elif str == ":-P":
+        print("stick out my tongue")
+    elif str == "(~.~)":
+        print("sleepy")
+    elif str == "TA":
+        print("totally awesome")
+    elif str == "CCC":
+        print("Canadian Computing Competition")
+    elif str == "CUZ":
+        print("because")
+    elif str == "TY":
+        print("thank-you")
+    elif str == "YW":
+        print("you’re welcome")
+    elif str == "TTYL":
+        print("talk to you later")
+        break
+    else:
+        print(str)
