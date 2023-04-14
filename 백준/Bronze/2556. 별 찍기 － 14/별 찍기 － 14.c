42e36ee6a51a980bd23f3257b6493a1f2763e7a7
JHPark0906 <91676766+JHPark0906@users.noreply.github.com>
2023-04-14 20:24:31 +0900

    [Bronze V] Title: 별 찍기 - 14, Time: 0 ms, Memory: 1112 KB -BaekjoonHub

---
+++ 백준/Bronze/2556. 별 찍기 － 14/별 찍기 － 14.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+
+int main(void) {
+    int size;
+    scanf("%d", &size);
+    for (int i = 0; i < size; i++){
+        for (int j = 0; j < size; j++) printf("*");
+        printf("\n");
+    }
+    return 0;
+}
