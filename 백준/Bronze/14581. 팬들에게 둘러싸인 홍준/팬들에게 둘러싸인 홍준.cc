38f630c224527c4963da99e3b2f91b86e53f5314
JHPark0906 <91676766+JHPark0906@users.noreply.github.com>
2023-05-21 21:13:47 +0900

    [Bronze V] Title: 팬들에게 둘러싸인 홍준, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/14581. 팬들에게 둘러싸인 홍준/팬들에게 둘러싸인 홍준.cc
@@ -0,0 +1,12 @@
+#include <iostream>
+
+#define STR_MAX 21
+
+using namespace std;
+
+int main() {
+    char name[STR_MAX];
+    cin >> name;
+    cout << ":fan::fan::fan:\n:fan::" << name << "::fan:\n:fan::fan::fan:";
+    return 0;
+}
